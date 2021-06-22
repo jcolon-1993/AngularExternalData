2b18f5236a07354dacfbb2d22febe6a4c256f67b
Jimmy Colon <60831920+jcolon-1993@users.noreply.github.com>
2021-06-22 16:28:40 -0700

    Update angular-external-data.js

--- js/angular-external-data.js
+++ js/angular-external-data.js
@@ -1,6 +1,12 @@
 function TimetableCtrl($scope, $http)
 {
+  /* The path to the JSON file is relative to the HTML
+  template*/
   $http.get("js/items.json")
+    /* If the request successfully fetches data, the code
+    in the success() function runs */
     .success(function(data) { $scope.sessions = data.sessions; })
+    /* If it fails, the error() function is run instead. This
+    would to show an error message to users  */
     .error(function(data) { alert("Page timed out") });
 }
